9c5e17b48e89675b69bc93b5c35f2a6839a46ac7
Christopher Durtschi <pherph@gmail.com>
2010-12-05 14:05:21 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DialALyric::Application.config.secret_token = 'b5c3f89d9f8a00a87228a0aadace5997bdaa26f45dc92f4a2d87d01a187ce4fb774c5356a47322205b5b071c12ac9f0ead542d6bf936612af03d2da7b7270e9b'
