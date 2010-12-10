2dc1aeb9624ba41a40475509f07889908166bbcf
Christopher Durtschi <pherph@gmail.com>
2010-12-10 11:33:57 -0700

    Limit actions on resources. Add show action for Lyric.

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -2,4 +2,8 @@ class LyricsController < ApplicationController
   def search
     @search = Lyric.search(params)
   end
+
+  def show
+    @lyric = Lyric.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 DialALyric::Application.routes.draw do
-  resources :calls do
+  resources :calls, :only => [:new, :create, :show] do
     post 'initiate', :on => :collection
   end
-  resources :lyrics do
+  resources :lyrics, :only => [:show] do
     get 'search', :on => :collection
   end
   root :to => "lyrics#search"
